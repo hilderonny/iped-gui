bbd033faf6986a37c94e0608036c9323f2fae56d
hilderonny <hilderonny2014@gmail.com>
2023-10-13 09:08:38 +0200

    WInForms - Version erstellt

---
+++ IPED Gui WinForms/MainForm.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace IPED_Gui_WinForms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            lblSpuren = new Label();
+            lstSpuren = new ListBox();
+            btnVerzeichnisHinzufuegen = new Button();
+            btnEntfernen = new Button();
+            lblAusgabeverzeichnis = new Label();
+            tbAusgabeverzeichnis = new TextBox();
+            btnAuswaehlen = new Button();
+            cbProjektErweitern = new CheckBox();
+            cbFortsetzen = new CheckBox();
+            cbPortabel = new CheckBox();
+            cbInternetdatenLaden = new CheckBox();
+            cbAudioTranslation = new CheckBox();
+            cbBildKlassifizierung = new CheckBox();
+            cbOcr = new CheckBox();
+            tbKonsole = new TextBox();
+            btnStarten = new Button();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            lblWarnung = new Label();
+            btnDateienHinzufuegen = new Button();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // lblSpuren
+            // 
+            lblSpuren.AutoSize = true;
+            lblSpuren.Location = new Point(3, 0);
+            lblSpuren.Name = "lblSpuren";
+            lblSpuren.Size = new Size(44, 15);
+            lblSpuren.TabIndex = 0;
+            lblSpuren.Text = "Spuren";
+            // 
+            // lstSpuren
+            // 
+            tableLayoutPanel1.SetColumnSpan(lstSpuren, 2);
+            lstSpuren.Dock = DockStyle.Fill;
+            lstSpuren.FormattingEnabled = true;
+            lstSpuren.ItemHeight = 15;
+            lstSpuren.Location = new Point(163, 3);
+            lstSpuren.Name = "lstSpuren";
+            tableLayoutPanel1.SetRowSpan(lstSpuren, 3);
+            lstSpuren.SelectionMode = SelectionMode.MultiExtended;
+            lstSpuren.Size = new Size(672, 154);
+            lstSpuren.TabIndex = 1;
+            lstSpuren.SelectedIndexChanged += lstSpuren_SelectedIndexChanged;
+            // 
+            // btnVerzeichnisHinzufuegen
+            // 
+            btnVerzeichnisHinzufuegen.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            btnVerzeichnisHinzufuegen.Location = new Point(841, 3);
+            btnVerzeichnisHinzufuegen.Name = "btnVerzeichnisHinzufuegen";
+            btnVerzeichnisHinzufuegen.Size = new Size(154, 23);
+            btnVerzeichnisHinzufuegen.TabIndex = 2;
+            btnVerzeichnisHinzufuegen.Text = "Verzeichnis hinzufügen";
+            btnVerzeichnisHinzufuegen.UseVisualStyleBackColor = true;
+            btnVerzeichnisHinzufuegen.Click += btnVerzeichnisHinzufuegen_Click;
+            // 
+            // btnEntfernen
+            // 
+            btnEntfernen.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            btnEntfernen.Enabled = false;
+            btnEntfernen.Location = new Point(841, 63);
+            btnEntfernen.Name = "btnEntfernen";
+            btnEntfernen.Size = new Size(154, 23);
+            btnEntfernen.TabIndex = 3;
+            btnEntfernen.Text = "Entfernen";
+            btnEntfernen.UseVisualStyleBackColor = true;
+            btnEntfernen.Click += btnEntfernen_Click;
+            // 
+            // lblAusgabeverzeichnis
+            // 
+            lblAusgabeverzeichnis.AutoSize = true;
+            lblAusgabeverzeichnis.Location = new Point(3, 160);
+            lblAusgabeverzeichnis.Name = "lblAusgabeverzeichnis";
+            lblAusgabeverzeichnis.Size = new Size(111, 15);
+            lblAusgabeverzeichnis.TabIndex = 4;
+            lblAusgabeverzeichnis.Text = "Ausgabeverzeichnis";
+            // 
+            // tbAusgabeverzeichnis
+            // 
+            tableLayoutPanel1.SetColumnSpan(tbAusgabeverzeichnis, 2);
+            tbAusgabeverzeichnis.Dock = DockStyle.Fill;
+            tbAusgabeverzeichnis.Location = new Point(163, 163);
+            tbAusgabeverzeichnis.Name = "tbAusgabeverzeichnis";
+            tbAusgabeverzeichnis.ReadOnly = true;
+            tbAusgabeverzeichnis.Size = new Size(672, 23);
+            tbAusgabeverzeichnis.TabIndex = 5;
+            // 
+            // btnAuswaehlen
+            // 
+            btnAuswaehlen.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            btnAuswaehlen.Location = new Point(841, 163);
+            btnAuswaehlen.Name = "btnAuswaehlen";
+            btnAuswaehlen.Size = new Size(154, 23);
+            btnAuswaehlen.TabIndex = 6;
+            btnAuswaehlen.Text = "Auswählen ...";
+            btnAuswaehlen.UseVisualStyleBackColor = true;
+            btnAuswaehlen.Click += btnAuswaehlen_Click;
+            // 
+            // cbProjektErweitern
+            // 
+            cbProjektErweitern.AutoSize = true;
+            cbProjektErweitern.Location = new Point(163, 253);
+            cbProjektErweitern.Name = "cbProjektErweitern";
+            cbProjektErweitern.Size = new Size(184, 19);
+            cbProjektErweitern.TabIndex = 8;
+            cbProjektErweitern.Text = "Bestehendes Projekt erweitern";
+            cbProjektErweitern.UseVisualStyleBackColor = true;
+            cbProjektErweitern.CheckedChanged += cbProjektErweitern_CheckedChanged;
+            // 
+            // cbFortsetzen
+            // 
+            cbFortsetzen.AutoSize = true;
+            cbFortsetzen.Location = new Point(163, 283);
+            cbFortsetzen.Name = "cbFortsetzen";
+            cbFortsetzen.Size = new Size(158, 19);
+            cbFortsetzen.TabIndex = 9;
+            cbFortsetzen.Text = "Nach Abbruch fortsetzen";
+            cbFortsetzen.UseVisualStyleBackColor = true;
+            cbFortsetzen.CheckedChanged += cbFortsetzen_CheckedChanged;
+            // 
+            // cbPortabel
+            // 
+            cbPortabel.AutoSize = true;
+            cbPortabel.Location = new Point(163, 313);
+            cbPortabel.Name = "cbPortabel";
+            cbPortabel.Size = new Size(70, 19);
+            cbPortabel.TabIndex = 10;
+            cbPortabel.Text = "Portabel";
+            cbPortabel.UseVisualStyleBackColor = true;
+            cbPortabel.CheckedChanged += cbPortabel_CheckedChanged;
+            // 
+            // cbInternetdatenLaden
+            // 
+            cbInternetdatenLaden.AutoSize = true;
+            cbInternetdatenLaden.Location = new Point(163, 343);
+            cbInternetdatenLaden.Name = "cbInternetdatenLaden";
+            cbInternetdatenLaden.Size = new Size(129, 19);
+            cbInternetdatenLaden.TabIndex = 11;
+            cbInternetdatenLaden.Text = "Internetdaten laden";
+            cbInternetdatenLaden.UseVisualStyleBackColor = true;
+            cbInternetdatenLaden.CheckedChanged += cbInternetdatenLaden_CheckedChanged;
+            // 
+            // cbAudioTranslation
+            // 
+            cbAudioTranslation.AutoSize = true;
+            cbAudioTranslation.Location = new Point(502, 253);
+            cbAudioTranslation.Name = "cbAudioTranslation";
+            cbAudioTranslation.Size = new Size(222, 19);
+            cbAudioTranslation.TabIndex = 12;
+            cbAudioTranslation.Text = "Audio Transkription und Übersetzung";
+            cbAudioTranslation.UseVisualStyleBackColor = true;
+            cbAudioTranslation.CheckedChanged += cbAudioTranslation_CheckedChanged;
+            // 
+            // cbBildKlassifizierung
+            // 
+            cbBildKlassifizierung.AutoSize = true;
+            cbBildKlassifizierung.Location = new Point(502, 283);
+            cbBildKlassifizierung.Name = "cbBildKlassifizierung";
+            cbBildKlassifizierung.Size = new Size(125, 19);
+            cbBildKlassifizierung.TabIndex = 13;
+            cbBildKlassifizierung.Text = "Bilder klassifizieren";
+            cbBildKlassifizierung.UseVisualStyleBackColor = true;
+            cbBildKlassifizierung.CheckedChanged += cbBildKlassifizierung_CheckedChanged;
+            // 
+            // cbOcr
+            // 
+            cbOcr.AutoSize = true;
+            cbOcr.Location = new Point(502, 313);
+            cbOcr.Name = "cbOcr";
+            cbOcr.Size = new Size(257, 19);
+            cbOcr.TabIndex = 14;
+            cbOcr.Text = "Texterkennung in Bildern und PDFs per OCR";
+            cbOcr.UseVisualStyleBackColor = true;
+            cbOcr.CheckedChanged += cbOcr_CheckedChanged;
+            // 
+            // tbKonsole
+            // 
+            tableLayoutPanel1.SetColumnSpan(tbKonsole, 2);
+            tbKonsole.Dock = DockStyle.Fill;
+            tbKonsole.Location = new Point(163, 373);
+            tbKonsole.Multiline = true;
+            tbKonsole.Name = "tbKonsole";
+            tbKonsole.Size = new Size(672, 192);
+            tbKonsole.TabIndex = 15;
+            // 
+            // btnStarten
+            // 
+            btnStarten.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            btnStarten.Location = new Point(841, 542);
+            btnStarten.Name = "btnStarten";
+            btnStarten.Size = new Size(154, 23);
+            btnStarten.TabIndex = 16;
+            btnStarten.Text = "Starten";
+            btnStarten.UseVisualStyleBackColor = true;
+            btnStarten.Click += btnStarten_Click;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 4;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 160F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 160F));
+            tableLayoutPanel1.Controls.Add(lblSpuren, 0, 0);
+            tableLayoutPanel1.Controls.Add(cbOcr, 2, 7);
+            tableLayoutPanel1.Controls.Add(tbKonsole, 1, 9);
+            tableLayoutPanel1.Controls.Add(cbBildKlassifizierung, 2, 6);
+            tableLayoutPanel1.Controls.Add(cbAudioTranslation, 2, 5);
+            tableLayoutPanel1.Controls.Add(lstSpuren, 1, 0);
+            tableLayoutPanel1.Controls.Add(cbInternetdatenLaden, 1, 8);
+            tableLayoutPanel1.Controls.Add(btnVerzeichnisHinzufuegen, 3, 0);
+            tableLayoutPanel1.Controls.Add(cbPortabel, 1, 7);
+            tableLayoutPanel1.Controls.Add(btnEntfernen, 3, 2);
+            tableLayoutPanel1.Controls.Add(cbFortsetzen, 1, 6);
+            tableLayoutPanel1.Controls.Add(lblAusgabeverzeichnis, 0, 3);
+            tableLayoutPanel1.Controls.Add(cbProjektErweitern, 1, 5);
+            tableLayoutPanel1.Controls.Add(tbAusgabeverzeichnis, 1, 3);
+            tableLayoutPanel1.Controls.Add(btnAuswaehlen, 3, 3);
+            tableLayoutPanel1.Controls.Add(btnStarten, 3, 9);
+            tableLayoutPanel1.Controls.Add(lblWarnung, 1, 4);
+            tableLayoutPanel1.Controls.Add(btnDateienHinzufuegen, 3, 1);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(5, 5);
+            tableLayoutPanel1.Margin = new Padding(0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 10;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 100F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 60F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Size = new Size(998, 568);
+            tableLayoutPanel1.TabIndex = 17;
+            // 
+            // lblWarnung
+            // 
+            lblWarnung.AutoSize = true;
+            tableLayoutPanel1.SetColumnSpan(lblWarnung, 2);
+            lblWarnung.Dock = DockStyle.Fill;
+            lblWarnung.ForeColor = Color.Red;
+            lblWarnung.Location = new Point(163, 190);
+            lblWarnung.Name = "lblWarnung";
+            lblWarnung.Size = new Size(672, 60);
+            lblWarnung.TabIndex = 17;
+            // 
+            // btnDateienHinzufuegen
+            // 
+            btnDateienHinzufuegen.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            btnDateienHinzufuegen.Location = new Point(841, 33);
+            btnDateienHinzufuegen.Name = "btnDateienHinzufuegen";
+            btnDateienHinzufuegen.Size = new Size(154, 23);
+            btnDateienHinzufuegen.TabIndex = 18;
+            btnDateienHinzufuegen.Text = "Dateien hinzufügen";
+            btnDateienHinzufuegen.UseVisualStyleBackColor = true;
+            btnDateienHinzufuegen.Click += btnDateienHinzufuegen_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1008, 578);
+            Controls.Add(tableLayoutPanel1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "MainForm";
+            Padding = new Padding(5);
+            Text = "IPED GUI";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label lblSpuren;
+        private ListBox lstSpuren;
+        private Button btnVerzeichnisHinzufuegen;
+        private Button btnEntfernen;
+        private Label lblAusgabeverzeichnis;
+        private TextBox tbAusgabeverzeichnis;
+        private Button btnAuswaehlen;
+        private CheckBox cbProjektErweitern;
+        private CheckBox cbFortsetzen;
+        private CheckBox cbPortabel;
+        private CheckBox cbInternetdatenLaden;
+        private CheckBox cbAudioTranslation;
+        private CheckBox cbBildKlassifizierung;
+        private CheckBox cbOcr;
+        private TextBox tbKonsole;
+        private Button btnStarten;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label lblWarnung;
+        private Button btnDateienHinzufuegen;
+    }
+}
---
+++ IPED Gui WinForms/MainForm.cs
@@ -0,0 +1,295 @@
+using IPED_Gui_WinForms.Properties;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace IPED_Gui_WinForms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            synchronizationContext = SynchronizationContext.Current;
+            InitializeComponent();
+            LoadSettings();
+            CheckForWarning();
+        }
+
+        private SynchronizationContext? synchronizationContext;
+
+        private void CheckForWarning()
+        {
+            lblWarnung.Text = string.Empty;
+            string outputPath = tbAusgabeverzeichnis.Text;
+            if (outputPath.Length == 0)
+            {
+                lblWarnung.Text = "Bitte geben Sie ein Ausgabeverzeichnis an!";
+                btnStarten.Enabled = false;
+                return;
+            }
+            if (!Directory.Exists(outputPath))
+            {
+                lblWarnung.Text = "Das Ausgabeverzeichnis existiert nicht (mehr). Bitte wählen Sie ein existierendes Verzeichnis!";
+                btnStarten.Enabled = false;
+                return;
+            }
+            if (!cbProjektErweitern.Checked && !cbFortsetzen.Checked && Directory.GetFileSystemEntries(outputPath).Any())
+            {
+                lblWarnung.Text = "Das Ausgabeverzeichnis ist nicht leer. Soll das bestehende Projekt erweitert oder ein vorher abgebrochener Vorgang fortgesetzt werden?";
+                return;
+            }
+        }
+
+        private string CreateIpedArguments()
+        {
+            List<string> argumentParts = new List<string>();
+            if (cbFortsetzen.Checked) argumentParts.Add("--continue");
+            if (cbInternetdatenLaden.Checked) argumentParts.Add("--downloadInternetData");
+            if (cbPortabel.Checked) argumentParts.Add("--portable");
+            if (cbProjektErweitern.Checked) argumentParts.Add("--append");
+            foreach (var path in lstSpuren.Items)
+            {
+                argumentParts.Add("-d");
+                argumentParts.Add("\"" + path + "\"");
+            }
+            argumentParts.Add("-o");
+            argumentParts.Add("\"" + tbAusgabeverzeichnis.Text + "\"");
+            return string.Join(" ", argumentParts);
+        }
+
+        private string CreateIPEDConfig()
+        {
+            List<string> configLines = new List<string>
+            {
+                "enableHash = true",
+                "enablePhotoDNA = false",
+                "enableHashDBLookup = true",
+                "enablePhotoDNALookup = false",
+                "enableLedDie = true",
+                "enableYahooNSFWDetection = false",
+                "enableQRCode = true",
+                "ignoreDuplicates = false",
+                "exportFileProps = false",
+                "processFileSignatures = true",
+                "enableFileParsing = true",
+                "expandContainers = true",
+                "processEmbeddedDisks = true",
+                "enableRegexSearch = true",
+                "enableAutomaticExportFiles = true",
+                "enableLanguageDetect = true",
+                "enableNamedEntityRecogniton = false",
+                "enableGraphGeneration = true",
+                "entropyTest = true",
+                "indexFileContents = true",
+                "enableIndexToElasticSearch = false",
+                "enableMinIO = false",
+                "enableAudioTranscription = false",
+                "enableCarving = true",
+                "enableLedCarving = false",
+                "enableKnownMetCarving = false",
+                "enableImageThumbs = true",
+                "enableImageSimilarity = true",
+                "enableFaceRecognition = true",
+                "enableVideoThumbs = true",
+                "enableDocThumbs = true",
+                "enableHTMLReport = true",
+                "enableAudioTranslation = " + (cbAudioTranslation.Checked ? "true" : "false"),
+                "enableTextTranslation = false",
+                "enableImageClassification = " + (cbBildKlassifizierung.Checked ? "true" : "false")
+            };
+            return string.Join("\n", configLines);
+        }
+
+        private string CreateOcrConfig()
+        {
+            List<string> configLines = new List<string>
+            {
+                "enableOCR = " + (cbOcr.Checked ? "true" : "false"),
+                "OCRLanguage = eng+deu",
+                "pageSegMode = 1",
+                "minFileSize2OCR = 1000",
+                "maxFileSize2OCR = 200000000",
+                "pdfToImgResolution = 250",
+                "maxPDFTextSize2OCR = 100",
+                "pdfToImgLib = icepdf",
+                "externalPdfToImgConv = true",
+                "externalConvMaxMem = 512M",
+                "processNonStandard = false",
+                "maxConvImageSize = 3000"
+            };
+            return string.Join("\n", configLines);
+        }
+
+        private void LoadSettings()
+        {
+            Settings settings = Settings.Default;
+            tbAusgabeverzeichnis.Text = settings.General_Output_Directory;
+            cbProjektErweitern.Checked = settings.General_Append;
+            cbFortsetzen.Checked = settings.General_Continue;
+            cbPortabel.Checked = settings.General_Portable;
+            cbInternetdatenLaden.Checked = settings.General_Download_Internet_Data;
+            cbAudioTranslation.Checked = settings.IPEDConfig_enableAudioTranslation;
+            cbBildKlassifizierung.Checked = settings.IPEDConfig_enableImageClassification;
+            cbOcr.Checked = settings.General_Ocr;
+        }
+
+        private void WriteToConsole(string? line)
+        {
+            tbKonsole.AppendText(line + "\n");
+        }
+
+        private void cbProjektErweitern_CheckedChanged(object sender, EventArgs e)
+        {
+            Settings settings = Settings.Default;
+            if (cbProjektErweitern.Checked)
+            {
+                cbFortsetzen.Checked = false;
+                settings.General_Continue = false;
+            }
+            settings.General_Append = cbProjektErweitern.Checked;
+            settings.Save();
+            CheckForWarning();
+        }
+
+        private void cbFortsetzen_CheckedChanged(object sender, EventArgs e)
+        {
+            Settings settings = Settings.Default;
+            if (cbFortsetzen.Checked)
+            {
+                cbProjektErweitern.Checked = false;
+                settings.General_Append = false;
+            }
+            settings.General_Continue = cbFortsetzen.Checked;
+            settings.Save();
+            CheckForWarning();
+        }
+
+        private void cbPortabel_CheckedChanged(object sender, EventArgs e)
+        {
+            Settings settings = Settings.Default;
+            settings.General_Portable = cbPortabel.Checked;
+            settings.Save();
+        }
+
+        private void cbInternetdatenLaden_CheckedChanged(object sender, EventArgs e)
+        {
+            Settings settings = Settings.Default;
+            settings.General_Download_Internet_Data = cbInternetdatenLaden.Checked;
+            settings.Save();
+        }
+
+        private void cbAudioTranslation_CheckedChanged(object sender, EventArgs e)
+        {
+            Settings settings = Settings.Default;
+            settings.IPEDConfig_enableAudioTranslation = cbAudioTranslation.Checked;
+            settings.Save();
+        }
+
+        private void cbBildKlassifizierung_CheckedChanged(object sender, EventArgs e)
+        {
+            Settings settings = Settings.Default;
+            settings.IPEDConfig_enableImageClassification = cbBildKlassifizierung.Checked;
+            settings.Save();
+        }
+
+        private void cbOcr_CheckedChanged(object sender, EventArgs e)
+        {
+            Settings settings = Settings.Default;
+            settings.General_Ocr = cbOcr.Checked;
+            settings.Save();
+        }
+
+        private void btnAuswaehlen_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.InitialDirectory = tbAusgabeverzeichnis.Text;
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                tbAusgabeverzeichnis.Text = folderBrowserDialog.SelectedPath;
+                Settings settings = Settings.Default;
+                settings.General_Output_Directory = folderBrowserDialog.SelectedPath;
+                settings.Save();
+                CheckForWarning();
+            }
+        }
+
+        private void btnVerzeichnisHinzufuegen_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                lstSpuren.Items.Add(folderBrowserDialog.SelectedPath);
+            }
+        }
+
+        private void btnDateienHinzufuegen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = true;
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string fileName in fileDialog.FileNames)
+                {
+                    lstSpuren.Items.Add(fileName);
+                }
+            }
+        }
+
+        private void lstSpuren_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnEntfernen.Enabled = lstSpuren.SelectedItems.Count > 0;
+        }
+
+        private void btnEntfernen_Click(object sender, EventArgs e)
+        {
+            ListBox.SelectedObjectCollection selectedItems = lstSpuren.SelectedItems;
+            for (int i = selectedItems.Count - 1; i >= 0; i--)
+            {
+                lstSpuren.Items.Remove(selectedItems[i]);
+            }
+        }
+
+        private void btnStarten_Click(object sender, EventArgs e)
+        {
+            Settings settings = Settings.Default;
+
+            string cfgIPEDConfig = CreateIPEDConfig();
+            File.WriteAllText(settings.General_Iped_Exe_Path + settings.General_Relative_IPEDConfig_File_Path, cfgIPEDConfig);
+
+            string cfgOcrConfig = CreateOcrConfig();
+            File.WriteAllText(settings.General_Iped_Exe_Path + settings.General_Relative_OCRConfig_File_Path, cfgOcrConfig);
+
+            string ipedCommand = Settings.Default.General_Iped_Exe_Path + "jre\\bin\\java.exe";
+            string ipedArguments = " -jar " + Settings.Default.General_Iped_Exe_Path + "iped.jar " + CreateIpedArguments();
+            tbKonsole.Clear();
+            WriteToConsole(ipedCommand + " " + ipedArguments + "\n");
+
+            using (var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = ipedCommand,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    Arguments = ipedArguments,
+                }
+            })
+            {
+                btnStarten.Enabled = false;
+
+                process.OutputDataReceived += (sender, args) => synchronizationContext?.Post(_ => WriteToConsole(args.Data), null);
+                process.ErrorDataReceived += (sender, args) => synchronizationContext?.Post(_ => WriteToConsole(args.Data), null);
+
+                process.Start();
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+
+                process.WaitForExit();
+
+                btnStarten.Enabled = true;
+
+            }
+        }
+    }
+}
