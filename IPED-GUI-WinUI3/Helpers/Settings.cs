2dc1d2e86af2fdede102c811f5df0870e6695e8c
hilderonny <hilderonny2014@gmail.com>
2023-11-16 14:46:17 +0100

    Prepared settings profiles with Newtonsoft.JSON

---
+++ IPED-GUI-WinUI3/Helpers/Settings.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace IPED_GUI_WinUI3.Helpers
+{
+    internal class Settings
+    {
+
+        public string SETTINGS_IPEDEXEPATH;
+        public string SETTINGS_LOCALE;
+
+        public static Settings LoadFromFile(string filePath)
+        {
+            return JsonConvert.DeserializeObject<Settings>(File.ReadAllText(filePath));
+        }
+
+        public void Save(string filePath)
+        {
+            File.WriteAllText(filePath, JsonConvert.SerializeObject(this));
+        }
+
+    }
+}
